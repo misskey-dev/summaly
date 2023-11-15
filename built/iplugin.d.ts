d2a3e07205c3c9769bc5a7b42031c8884b5a25c8
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2023-11-15 19:23:03 +0900

    fix: branch.ioを用いたディープリンクをパースできるように修正 (#13)
    * (fix) branch.ioのディープリンクでうまく作動しないのを修正
    
    * Update changelog
    
    * fix regex

--- built/iplugin.d.ts
+++ built/iplugin.d.ts
@@ -3,5 +3,5 @@ import type { URL } from 'node:url';
 import Summary from './summary.js';
 export interface IPlugin {
     test: (url: URL) => boolean;
-    summarize: (url: URL, lang?: string) => Promise<Summary>;
+    summarize: (url: URL, lang?: string) => Promise<Summary | null>;
 }
--- built/plugins/index.js
+++ built/plugins/index.js
@@ -1,6 +1,8 @@
 import * as amazon from './amazon.js';
 import * as wikipedia from './wikipedia.js';
+import * as branchIoDeeplinks from './branchio-deeplinks.js';
 export const plugins = [
     amazon,
     wikipedia,
+    branchIoDeeplinks,
 ];
