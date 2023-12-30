38000dd462494c0e695dc1e39beae8504ac24c41
anatawa12 <anatawa12@icloud.com>
2023-12-30 15:33:43 +0900

    feat: support 'mixi:content-rating' for sensitive (#16)
    * feat: support 'mixi:content-rating' for sensitive
    
    * fix: d7f6a88aee62510a0d3ba061f515e4688c34ca48 でビルドしてない
    
    * ビルドする

--- src/general.ts
+++ src/general.ts
@@ -217,7 +217,10 @@ export default async (_url: URL | string, lang: string | null = null): Promise<S
 	const activityPub =
 		$('link[rel="alternate"][type="application/activity+json"]').attr('href') || null;
 
-	const sensitive = $('.tweet').attr('data-possibly-sensitive') === 'true'
+	// https://developer.mixi.co.jp/connect/mixi_plugin/mixi_check/spec_mixi_check/#toc-18-
+	const sensitive = 
+		$("meta[property='mixi:content-rating']").attr('content') == '1' ||
+		$('.tweet').attr('data-possibly-sensitive') === 'true'
 
 	const find = async (path: string) => {
 		const target = new URL(path, url.href);
