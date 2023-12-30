c45b3a1c63830304975bb5d2a273bc95ef642911
tamaina <tamaina@hotmail.co.jp>
2023-12-30 06:53:31 +0000

    add workflows, mod chglog, remove built

--- built/plugins/index.js
+++
@@ -1,8 +0,0 @@
-import * as amazon from './amazon.js';
-import * as wikipedia from './wikipedia.js';
-import * as branchIoDeeplinks from './branchio-deeplinks.js';
-export const plugins = [
-    amazon,
-    wikipedia,
-    branchIoDeeplinks,
-];
