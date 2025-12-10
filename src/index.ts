dd398be111badfbd53ee7fa368a974385ab6fa0c
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2025-12-10 12:10:19 +0900

    Update src/index.ts
    Co-authored-by: Kisaragi <48310258+KisaragiEffective@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -152,7 +152,7 @@ export default function (fastify: FastifyInstance, options: SummalyOptions, done
 			});
 		}
 
-		reply.header('Cache-Control', 'public, max-age=86400, stale-while-revalidate=43200, immutable');
+		reply.header('Cache-Control', 'public, max-age=86400, stale-while-revalidate=43200');
 
 		try {
 			const summary = await summaly(url, {
