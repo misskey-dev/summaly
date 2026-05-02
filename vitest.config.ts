6b4b7071cd7a9b0840177740f36235e8608ac4a7
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-05-02 16:19:54 +0900

    fix test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
 import { fileURLToPath } from 'node:url';
+import { readFileSync } from 'node:fs';
 import { defineConfig } from 'vitest/config';
+import { replacePlugin } from 'rolldown/plugins';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	plugins: [
+		replacePlugin({
+			_VERSION_: JSON.stringify(JSON.parse(readFileSync('./package.json', 'utf-8')).version),
+		}),
+	],
 });
