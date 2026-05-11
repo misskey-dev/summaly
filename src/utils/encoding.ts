620dbbf39c18390f9b2504e768d78fb1d1ff9b5f
かっこかり <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-05-11 21:42:15 +0900

    enhance: テキストのデコードにネイティブの `TextDecoder` を使用するように (#65)
    * enhance: テキストのデコードにネイティブのAPIを使用するように
    
    * perf
    
    * Update Changelog
    
    * fix lint

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -1,7 +1,9 @@
-import * as iconv from 'iconv-lite';
 import { detect } from 'chardet';
 
 const regCharset = new RegExp(/charset\s*=\s*["']?([\w-]+)/, 'i');
+
+// UTF-8は多くのサイトで採用されているため、インスタンスを使い回すことでパフォーマンスを向上させる
+const utf8TextDecoder = new TextDecoder('utf-8', { fatal: false });
 
 /**
  * Detect HTML encoding
@@ -11,32 +13,28 @@ const regCharset = new RegExp(/charset\s*=\s*["']?([\w-]+)/, 'i');
 export function detectEncoding(body: Uint8Array): string {
 	// By detection
 	const detected = detect(body);
-	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	if (detected) {
-		const encoding = toEncoding(detected);
-		if (encoding != null) return encoding;
+	if (detected != null) {
+		return detected.toLowerCase();
 	}
 
 	// From meta
 	const matchMeta = body.toString().match(regCharset);
 	if (matchMeta) {
 		const candicate = matchMeta[1];
-		const encoding = toEncoding(candicate);
-		if (encoding != null) return encoding;
+		if (candicate) {
+			return candicate.toLowerCase();
+		}
 	}
 
 	return 'utf-8';
 }
 
 export function toUtf8(body: Uint8Array, encoding: string): string {
-	return iconv.decode(body, encoding);
-}
-
-function toEncoding(candicate: string): string | null {
-	if (iconv.encodingExists(candicate)) {
-		if (['shift_jis', 'shift-jis', 'windows-31j', 'x-sjis'].includes(candicate.toLowerCase())) return 'cp932';
-		return candicate;
-	} else {
-		return null;
+	try {
+		const decoder = encoding === 'utf-8' ? utf8TextDecoder : new TextDecoder(encoding, { fatal: true });
+		return decoder.decode(body);
+	} catch {
+		// フォールバック
+		return utf8TextDecoder.decode(body);
 	}
 }
